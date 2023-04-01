31834500f8c9463adb8ea6e83e48b0d1554c17ad
Efe Agare <knowledgeagare157@gmail.com>
2023-04-01 21:09:13 +0100

    fix server by adding the global variable `data` to store the objects

--- server.rb
+++ server.rb
@@ -3,15 +3,24 @@
 require 'rack'
 require 'json'
 
-class Server
+require_relative './src/front_controller'
+require_relative './src/router/routes'
 
+Dir.glob("src/controllers/**/*.rb").each{ |file_path|
+  file_name = File.basename(file_path, ".rb")
+  require_relative "./#{file_path}"
+}
+
+
+class Server
   def call(env)
     # the request path
     path = env['PATH_INFO']
-    case env['REQUEST_METHOD']
-    when 'GET'
-      ['200', {}, ['']]
-    end
+
+    req = Rack::Request.new(env)
+    request_method = env['REQUEST_METHOD']
+    
+    FrontController.new.handle request_method, path, req
   end
 end
 
@@ -19,6 +28,9 @@ end
 # modifications needed here.
 if __FILE__ == $0
   app = Rack::Builder.new do
+    # Define a global hash table to store the objects
+    $data = {}
+
     use Rack::Reloader
     run Server.new
   end.to_app
